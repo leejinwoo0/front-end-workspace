34fb998d26f03006092360328ddd7fd3d2b40697
leejinwoo0 <2jw0910@naver.com>
2024-08-06 15:17:24 +0900

    0806

--- 3_JavaScript/basic/연습34.js
+++ 3_JavaScript/basic/연습34.js
@@ -10,7 +10,7 @@ const userPhone = document.querySelector("#userPhone");
 const userPhoneSpan = document.querySelector("#userPhoneSpan");
 
 userId.addEventListener("input", function (e) {
-  // 첫글자 반드시 영문자로, 영문자 숫자 포함하여 총 5~15자로 입력하시오
+  // 첫 글자 반드시 영문자로 시작하며, 영문자와 숫자를 포함하여 총 5~15자로 입력
   const regExp = /^[a-zA-Z][a-zA-Z0-9]{4,14}$/;
   const check = regExp.test(userId.value);
 
@@ -18,30 +18,30 @@ userId.addEventListener("input", function (e) {
     userIdSpan.style.color = "blue";
     userIdSpan.innerHTML = "OK!";
   } else {
-    userIdSpan.stle.color = "red";
+    userIdSpan.style.color = "red";
     userIdSpan.innerHTML =
       "첫글자 반드시 영문자로, 영문자 숫자 포함하여 총 5~15자로 입력하시오";
   }
 });
 
 userPwd.addEventListener("input", function (e) {
-  // 영문자, 숫자, 특수문자 포함하여 총 9~15자로 입력하시오
-  const regExp = /^[!-~]{9,15}$/;
-  const check = regExp.text(userPwd.value);
+  // 영문자, 숫자, 특수문자를 포함하여 9~15자로 입력
+  const regExp = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[!-~]{9,15}$/;
+  const check = regExp.test(userPwd.value);
 
   if (check) {
     userPwdSpan.style.color = "green";
-    userPWdSpan.innerHTML = "OK!";
+    userPwdSpan.innerHTML = "OK!";
   } else {
     userPwdSpan.style.color = "red";
-    userPWdSpan.innerHTML =
+    userPwdSpan.innerHTML =
       "영문자, 숫자, 특수문자 포함하여 총 9~15자로 입력하시오";
   }
 });
 
 userPwdCheck.addEventListener("input", function (e) {
-  // 위의 비밀번호와 일치하게 입력하시오
-  if (userPWd.value === userPwdCheck.value) {
+  // 위의 비밀번호와 일치해야 함
+  if (userPwd.value === userPwdCheck.value) {
     userPwdCheckSpan.style.color = "green";
     userPwdCheckSpan.innerHTML = "OK!";
   } else {
@@ -51,7 +51,7 @@ userPwdCheck.addEventListener("input", function (e) {
 });
 
 userName.addEventListener("input", function (e) {
-  // 한글로 작성, 2글자 이상 입력하시오
+  // 한글로 작성하고, 2글자 이상 입력해야 함
   const regExp = /^[가-힣]{2,}$/;
   const check = regExp.test(userName.value);
 
@@ -60,12 +60,12 @@ userName.addEventListener("input", function (e) {
     userNameSpan.innerHTML = "OK!";
   } else {
     userNameSpan.style.color = "red";
-    userNameSpan.innerHTML = "// 한글로 작성, 2글자 이상 입력하시오";
+    userNameSpan.innerHTML = "한글로 작성, 2글자 이상 입력하시오";
   }
 });
 
 userPhone.addEventListener("input", function (e) {
-  // 올바른 번호 11자리를 입력해 주세요
+  // 올바른 번호 11자리를 입력해야 함
   const regExp = /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/;
   const check = regExp.test(userPhone.value);
 
---
+++ 4_jQuery/05_효과.js
@@ -0,0 +1,46 @@
+// 1. show, hide, toggle
+$("#show").click(() => {
+  $("#img").show();
+});
+$("#hide").click(() => {
+  $("#img").hide(3000);
+});
+$("#toggle").click(() => {
+  $("#img").toggle(3000);
+});
+// 2. fadeIn, fadeout, fadeToggle, fadeTo
+$("#fadeIn").click(() => {
+  $("#img").fadeIn(3000);
+});
+$("#fadeOut").click(() => {
+  $("#img").fadeOut(3000);
+});
+$("#fadeToggle").click(() => {
+  $("#img").fadeToggle(3000);
+});
+$("#img").hover(
+  function () {
+    $(this).fadeTo(500, 0.5);
+  }, // mouseenter
+  function () {
+    $(this).fadeTo(500, 1);
+  } // mouseleave
+);
+
+// 3. slideDown, slideUp, slideToggle
+$(".menu").click((e) => {
+  let content = $(e.target).next();
+  /*
+  if (content.css("display") === "none") {
+    content.slideDown(500);
+  } else {
+    content.slideUp(500);
+  }*/
+  //content.slideToggle(500);
+
+  // 하나의 콘텐츠만 slideDown
+  $(".contents").slideUp();
+  if (contnt.css("display") === "none") {
+    content.slideDown(500);
+  }
+});
